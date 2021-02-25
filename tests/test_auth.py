361651b3865ad9b632883145e7ad9a46fb340315
Andrew Rapoza <andrapoza@gmail.com>
2021-02-24 23:08:44 -0500

    fix testing typos

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -3,7 +3,7 @@ from flask import g, session
 from flaskr.db import get_db
 
 
-def test_register(cleint, app):
+def test_register(client, app):
     assert client.get('/auth/register').status_code == 200
     response = client.post(
         '/auth/register', data={'username': 'a', 'password': 'a'}
--- tests/test_factory.py
+++ tests/test_factory.py
@@ -3,9 +3,8 @@ from flaskr import create_app
 
 def test_config():
     assert not create_app().testing
-    assert create_app({'TESTTING: True'}).testing
-
+    assert create_app({'TESTING': True}).testing
 
 def test_hello(client):
     response = client.get('/hello')
-    assert response.data == b'Hello, World'
+    assert response.data == b'Hello, World!'
