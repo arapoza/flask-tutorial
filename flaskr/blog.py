1ce2b835092a5ef5e785b6bfb71df8769cf11bcf
Andrew Rapoza <andrapoza@gmail.com>
2021-02-25 19:52:12 -0500

    like button and like counter

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -12,7 +12,7 @@ bp = Blueprint('blog', __name__)
 def index():
     db = get_db()
     posts = db.execute(
-        'SELECT p.id, title, body, created, author_id, username'
+        'SELECT p.id, title, body, created, author_id, username, likes'
         ' FROM post p JOIN user u ON p.author_id = u.id'
         ' ORDER BY created DESC'
     ).fetchall()
@@ -47,7 +47,7 @@ def create():
 
 def get_post(id, check_author=True):
     post = get_db().execute(
-        'SELECT p.id, title, body, created, author_id, username'
+        'SELECT p.id, title, body, created, author_id, username, likes'
         ' FROM post p JOIN user u ON p.author_id = u.id'
         ' WHERE p.id = ?',
         (id,)
@@ -102,5 +102,25 @@ def delete(id):
 
 @bp.route('/post/<int:id>/')
 def detail(id):
-    post = get_post(id)
+    post = get_post(id, False)
     return render_template('blog/detail.html', post=post)
+
+
+@bp.route('/like/<int:id>/')
+@login_required
+def like(id):
+    post = get_post(id, False)
+    likes = post['likes']
+    likes = likes + 1
+    db = get_db()
+    db.execute('UPDATE post SET likes = ?' 
+               ' WHERE id = ?', 
+               (likes, id))
+    db.commit()
+    posts = db.execute(
+        'SELECT p.id, title, body, created, author_id, username, likes'
+        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' ORDER BY created DESC'
+    ).fetchall()
+    return render_template('blog/index.html', posts=posts)
+    
